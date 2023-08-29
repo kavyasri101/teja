6e61c5abb75870a3d29c8ed96dc087759fb67439
kavyasri101 <mkavyasri18@gmail.com>
2023-08-29 15:15:32 +0530

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "heyyyy you kavyaaaaahh" );
+        System.out.println( "welcome kavya" );
     }
 }
